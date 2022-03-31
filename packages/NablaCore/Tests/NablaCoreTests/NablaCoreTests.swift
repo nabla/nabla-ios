8326b4e3550aeb3d39790be91f1177b8cdcbf422
Ahmed Mseddi <amseddi@users.noreply.github.com>
2022-03-31 15:33:59 +0200

    [iOS-SDK] Ignore vendors in SwiftFormat and start all tests in CI (#17855)
    * added appcode project files to gitignore
    
    * ignored vendors in swiftformat
    
    * Start all packages tests in jenkins
    
    * fixed failing tests

--- packages/NablaCore/Tests/NablaCoreTests/NablaCoreTests.swift
+++ packages/NablaCore/Tests/NablaCoreTests/NablaCoreTests.swift
@@ -1,11 +1,16 @@
 @testable import NablaCore
+import NablaUtils
 import XCTest
 
 final class NablaCoreTests: XCTestCase {
+    override func setUp() {
+        Resolver.shared.register(type: NablaUtils.self, NablaUtils())
+    }
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(NablaCore().text, "Hello, World!")
+        XCTAssertEqual(NablaCore().text, "Hello, World!\nWith Utils!")
     }
 }
--- packages/NablaUtils/Tests/NablaUtilsTests/NablaUtilsTests.swift
+++ packages/NablaUtils/Tests/NablaUtilsTests/NablaUtilsTests.swift
@@ -6,6 +6,6 @@ final class NablaUtilsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(NablaUtils().text, "Hello, World!")
+        XCTAssertEqual(NablaUtils().text, "With Utils!")
     }
 }
