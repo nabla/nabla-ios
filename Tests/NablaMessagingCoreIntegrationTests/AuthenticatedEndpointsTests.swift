ebd95c79cfc1f4b41b1c94e8e3d8f1dc627beb80
Tanguy Helesbeux <tgyhlsb@users.noreply.github.com>
2022-05-31 16:21:02 +0200

    [iOS SDK] Integration tests - Assert authenticated endpoints (#19536)
    # Pull Request
    
    - Related to: https://github.com/nabla/health/issues/19250
    
    ## Summary
    
    Creates tests that fail when authenticated methods are called before `NablaClient.authenticate(provider:)`.
    
    ## Test plan
    
    - Tests are green

---
+++ Tests/NablaMessagingCoreIntegrationTests/AuthenticatedEndpointsTests.swift
@@ -0,0 +1,87 @@
+import NablaMessagingCore
+import XCTest
+
+class AuthenticatedEndpointsTests: XCTestCase {
+    private var nablaClient: NablaClient!
+    private var messagingClient: NablaMessagingClient!
+    
+    override func setUp() {
+        super.setUp()
+        nablaClient = NablaClient(apiKey: "", name: "")
+        messagingClient = NablaMessagingClient(client: nablaClient)
+    }
+    
+    private func assertAuthenticationErrorHandler<T>(
+        file: StaticString = #filePath, line: UInt = #line
+    ) -> (Result<T, NablaError>) -> Void {
+        let handlerCalled = expectation(description: "Handler was called")
+        return { result in
+            switch result {
+            case let .failure(error):
+                switch error {
+                case let .authenticationError(reason):
+                    switch reason {
+                    case .missingAuthenticationProvider:
+                        break
+                    default:
+                        XCTFail("Expected `AuthenticationError.missingAuthenticationProvider`, received \(reason)", file: file, line: line)
+                    }
+                default:
+                    XCTFail("Expected `NablaError.authenticationError`, received \(error)", file: file, line: line)
+                }
+            case .success:
+                XCTFail("Should not succeed", file: file, line: line)
+            }
+            handlerCalled.fulfill()
+        }
+    }
+    
+    func testWatchConversationsFailsWhenNotAuthenticated() {
+        _ = messagingClient.watchConversations(handler: assertAuthenticationErrorHandler())
+        waitForExpectations(timeout: 0.5)
+    }
+
+    func testCreateConversationFailsWhenNotAuthenticated() {
+        _ = messagingClient.createConversation(handler: assertAuthenticationErrorHandler())
+        waitForExpectations(timeout: 0.5)
+    }
+    
+    func testWatchConversationFailsWhenNotAuthenticated() {
+        _ = messagingClient.watchConversation(.init(), handler: assertAuthenticationErrorHandler())
+        waitForExpectations(timeout: 0.5)
+    }
+    
+    func testWatchItemsOfConversationWithIdFailsWhenNotAuthenticated() {
+        _ = messagingClient.watchItems(ofConversationWithId: .init(), handler: assertAuthenticationErrorHandler())
+        waitForExpectations(timeout: 0.5)
+    }
+    
+    func testMarkConversationAsSeenFailsWhenNotAuthenticated() {
+        _ = messagingClient.markConversationAsSeen(.init(), handler: assertAuthenticationErrorHandler())
+        waitForExpectations(timeout: 0.5)
+    }
+    
+    func testSetIsTypingFailsWhenNotAuthenticated() {
+        _ = messagingClient.setIsTyping(true, inConversationWithId: .init(), handler: assertAuthenticationErrorHandler())
+        waitForExpectations(timeout: 0.5)
+    }
+    
+    func testSendMessageInConversationWithIdFailsWhenNotAuthenticated() {
+        _ = messagingClient.sendMessage(
+            .text(content: "Hello world!"),
+            inConversationWithId: .init(),
+            handler: assertAuthenticationErrorHandler()
+        )
+        waitForExpectations(timeout: 0.5)
+    }
+    
+    func testRetrySendingItemWithIdInConversationWithIdFailsWhenNotAuthenticated() {
+        _ = messagingClient.retrySending(itemWithId: .init(), inConversationWithId: .init(), handler: assertAuthenticationErrorHandler())
+        waitForExpectations(timeout: 0.5)
+    }
+    
+    func testDeleteMessageWithIdInConversationWithIdFailsWhenNotAuthenticated() {
+        _ = messagingClient.deleteMessage(withId: .init(), conversationId: .init(), handler: assertAuthenticationErrorHandler())
+        waitForExpectations(timeout: 0.5)
+    }
+}
